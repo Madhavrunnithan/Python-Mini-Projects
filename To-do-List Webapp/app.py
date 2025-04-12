3c581098a0b3173eebbe91c728fa38598174962d
Madhavrunnithan <madhavrunnithan555@gmail.com>
2025-04-12 17:20:06 +0530

    third commit

---
+++ To-do-List Webapp/app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+tasklist = []
+
+@app.route("/")
+def home():
+    return render_template("index.html",task = tasklist)
+
+@app.route("/addtask",methods = ["POST"])
+def addtask():
+    if request.method == "POST":
+        task = request.form["task"]
+        tasklist.append(task)
+        print(tasklist)
+    
+    return render_template("index.html",task = tasklist)
+
+@app.route("/clear",methods = ["POST"])
+def clear():
+    if request.method == "POST":
+        tasklist.pop()
+    return render_template("index.html",task = tasklist)
+
+@app.route("/clearall",methods = ["POST"])
+def clearall():
+    if request.method == "POST":
+        tasklist.clear()
+        print(tasklist)
+    return render_template("index.html",task = tasklist)
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
